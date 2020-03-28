3f301d5f18b992a5ced94910f3b78fe58d64904d
jonghyuk lee <jonghyuklee@Johns-MBP.fios-router.home>
2020-03-28 13:17:58 -0400

    up to lesson 7

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// Check email valid
+function isValidEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -21,5 +27,25 @@ form.addEventListener('submit', function(e) {
     showError(username, 'Username is required');
   } else {
     showSuccess(username);
+  }
+
+  if (email.value == '') {
+    showError(email, 'Email is required');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value == '') {
+    //alert('Username is required');
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value == '') {
+    //alert('Username is required');
+    showError(password2, 'Password 2 is required');
+  } else {
+    showSuccess(password2);
   }
 });
