cd0780665caf887439ad07c3bdb00a17ed17ffe1
jonghyuk lee <jonghyuklee@Johns-MBP.fios-router.home>
2020-03-30 19:00:58 -0400

    final commit

--- script.js
+++ script.js
@@ -12,17 +12,74 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
 // Check email valid
-function isValidEmail(email) {
+/*function isValidEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
+}*/
+
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      //showError(input, `${input.id} is required`);
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Password do not match');
+  }
+}
+
+// Get fieldname
+function getFieldName(input) {
+  //return input.id.toUpperCase();
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // Event listeners
 form.addEventListener('submit', function(e) {
-  e.preventDefault();
+  e.preventDefault();  
 
-  if (username.value == '') {
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+
+  /*if (username.value == '') {
     //alert('Username is required');
     showError(username, 'Username is required');
   } else {
@@ -31,6 +88,8 @@ form.addEventListener('submit', function(e) {
 
   if (email.value == '') {
     showError(email, 'Email is required');
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid');
   } else {
     showSuccess(email);
   }
@@ -47,5 +106,5 @@ form.addEventListener('submit', function(e) {
     showError(password2, 'Password 2 is required');
   } else {
     showSuccess(password2);
-  }
+  }*/
 });
